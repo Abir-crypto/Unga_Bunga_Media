84646b53c7a60b7ef3ea226c69d54d723994723e
Abir-crypto <abdullahall.abir48@gmail.com>
2022-07-22 15:37:57 +0600

    created migration, model and controller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,19 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['first_name', 'last_name', 'username', 'email', 'password'];
+    protected $fillable = ['title', 'content'];
 
-    public function post(): HasMany{
-        return $this->hasMany(Post::class);
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
     }
 
-    public function comments(): HasMany{
+    public function comments():HasMany{
         return $this->hasMany(Comment::class);
     }
 }
